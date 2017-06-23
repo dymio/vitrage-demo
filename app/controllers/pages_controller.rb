f90ce9fa7c315b1cac542e0de2693a56bddefbf7
Ivan Dymkov <mstrdymio@gmail.com>
2017-06-23 16:12:50 +0700

    Add styles to the project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,6 +16,10 @@ class PagesController < ApplicationController
     @page = Page.new(uip: request.remote_ip)
 
     if @page.save
+      @page.vitrage_slots.create(piece: VitragePieces::VtrgText.create(body: "\
+        <h1>Welcome to your page!</h1>\
+        <p>Enter <a href=\"#{edit_page_path(@page.slug)}\">edit mode</a> \
+        to change content using Vitrage.</p>"))
       redirect_to page_path(@page.slug)
     else
       redirect_to root_path
